77e4aec1cea2c0e1b9baf01476f34e756f2bc761
Shamil0Zamaliev <114811430+Shamil0Zamaliev@users.noreply.github.com>
2022-12-26 05:46:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static string isAnagram(string string1, string string2)
+        {
+            char[] s1 = ((string1.Replace(" ", "")).ToCharArray());
+            char[] s2 = ((string2.Replace(" ", "")).ToCharArray());
+            Array.Sort(s1);
+            Array.Sort(s2);
+            if (s1.SequenceEqual(s2))
+                return "Yes";
+            else
+                return "No";
+        }
+        static void Main(string[] args)
+        {
+            string string1 = Console.ReadLine();
+            string string2 = Console.ReadLine();
+            Console.WriteLine(isAnagram(string1, string2));
+        }
+    }
+}
